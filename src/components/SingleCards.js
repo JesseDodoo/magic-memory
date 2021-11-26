f5fb52c3749514a235ee3eddc6d81aadb19300c0
JesseDodoo <jesse.dodoo@gmail.com>
2021-11-26 16:10:45 +0000

    first commit

---
+++ src/components/SingleCards.js
@@ -0,0 +1,27 @@
+import './SingleCards.css'
+
+export default function SingleCards({card, handleChoice, flipped, disabled}) {
+
+
+    const handleClick = () => {
+      if (!disabled) {
+        handleChoice(card)
+      }
+    }
+
+    return (
+        
+        <div className="card">
+          <div className={flipped ? "flipped" : ""}>
+            <img className="front" src={card.src} alt="card front" />
+            <img className="back" src="img/cover.png" onClick={handleClick}
+             alt="card back" />
+          </div>
+        </div>
+    
+      
+    
+    
+    )
+}
+
